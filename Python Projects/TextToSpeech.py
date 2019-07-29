9755e2bf9ab82fbac17f0372528bcceb416fd905
Mohamed Ali <35882218+mohmdbhbh99@users.noreply.github.com>
2019-07-30 01:07:11 +0200

    Add files via upload

--- Python Projects/TextToSpeech.py
+++ Python Projects/TextToSpeech.py
@@ -1,11 +1,10 @@
-import pyttsx3
+def recursion(k):
+  if(k>0):
+    result = k+recursion(k-1)
+    print(result)
+  else:
+    result = 0
+  return result
 
-engine = pyttsx3.init()
-
-x = str(input())
-if x == "hello":
-    engine.say('Hello mr Karius....')
-elif x == "how are you" :
-    engine.say('I am fine ..... thank you for your caring about me')
-
-engine.runAndWait()
+print("\n\nRecursion Example Results")
+recursion(6)
